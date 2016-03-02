eda6785c09ffdadc321d2b3e6ee5f69459075f68
dmitriy-serdyuk <serdyuk.dmitriy@gmail.com>
2016-03-02 11:12:45 -0500

    Move flushing functional to the JSONLog class

--- blocks/log/json.py
+++ blocks/log/json.py
@@ -18,36 +18,46 @@ def pretty_serialize_numpy(obj):
 class PicklableLogger(_Logger):
     """A picklable wrapper around mimir logger.
 
-    This class is a picklable version of `:class:mimir.Logger` with
-    some additional functionality. The current status is saved in
-    `iteration_status` attribute and is flushed to the file only if
-    the next iteration is requested or the class is pickled.
+    This class is a picklable version of `:class:mimir.Logger`.
 
     """
-    def __init__(self, filename, **kwargs):
-        logger = Logger(filename, default=pretty_serialize_numpy, **kwargs)
+    def __init__(self, **kwargs):
+        logger = Logger(**kwargs)
         self.__dict__.update(logger.__dict__)
-        self.all_kwargs = kwargs
-        self.all_kwargs['filename'] = filename
-        self.status = {}
-        self.iteration_status = {}
-        self.last_flushed = -1
+        self.logger_kwargs = kwargs
 
     def __setstate__(self, state):
         logger = Logger(**state)
         self.__dict__.update(logger.__dict__)
 
     def __getstate__(self):
-        self.flush()
-        return self.all_kwargs
+        return self.logger_kwargs
+
+
+class JSONLog(TrainingLogBase):
+    """JSON Lines log.
+
+    The current status is saved in
+    `iteration_status` attribute and is flushed to the file only if
+    the next iteration is requested.
+
+    """
+    def __init__(self, filename='log.jsonl.gz', **kwargs):
+        self.status = {}
+        TrainingLogBase.__init__(self)
+        self.logger = PicklableLogger(maxlen=2, filename=filename,
+                                      default=pretty_serialize_numpy, **kwargs)
+        self.last_flushed = -1
+        self.iteration_status = {}
 
     def flush(self):
         iterations_done = self.status['iterations_done']
-        self.log({iterations_done: self.iteration_status})
+        self.logger.log({iterations_done: self.iteration_status})
         self.iteration_status = {}
         self.last_flushed = iterations_done
 
-    def get_record(self, time):
+    def __getitem__(self, time):
+        self._check_time(time)
         iterations_done = self.status.get('iterations_done', -1)
         if time > self.last_flushed:
             self.flush()
@@ -57,21 +67,7 @@ class PicklableLogger(_Logger):
         elif time == iterations_done:
             return self.iteration_status
         else:
-            return self[iterations_done - time]
-
-
-class JSONLog(TrainingLogBase):
-    def __init__(self, filename='test.jsonl.gz'):
-        self.logger = PicklableLogger(maxlen=2, filename=filename)
-        TrainingLogBase.__init__(self)
-
-    @property
-    def status(self):
-        return self.logger.status
-
-    def __getitem__(self, time):
-        self._check_time(time)
-        return self.logger.get_record(time)
+            return self.logger[iterations_done - time]
 
     def __setitem__(self, time, value):
         raise ValueError('cannot manually change log')
